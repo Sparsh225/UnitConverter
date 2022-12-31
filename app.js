b551ed789d86578cdf513fb35ecc7e96fd51c31f
sparshrawat34@gmail.com <sparshrawat34@gamil.com>
2022-12-31 17:43:05 +0530

    Kilograms to Pounds Conversion

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+//Celsius to Fahrenheit Conversion
 function handleclick(){
     var ce=document.getElementById('c');
     var value=ce.value;
@@ -9,3 +9,14 @@ function handleclick(){
 }
 var b=document.getElementById('temperature');
 b.addEventListener('click',handleclick);
+
+//Kilograms to Pounds Conversion
+function hc2(){
+    var ke=document.getElementById('kg');
+    var val=ke.value;
+    val= val * 2.205;
+    var wee=document.getElementById('lbs');
+    wee.value=val;
+}
+var c=document.getElementById('weight');
+c.addEventListener('click',hc2);
