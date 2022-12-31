c4b4e1e02a397b70bea60009c08645636925dda6
sparshrawat34@gmail.com <sparshrawat34@gamil.com>
2022-12-31 21:56:44 +0530

    Kilometer to Mile Conversion

--- app.js
+++ app.js
@@ -13,10 +13,22 @@ b.addEventListener('click',handleclick);
 //Kilograms to Pounds Conversion
 function hc2(){
     var ke=document.getElementById('kg');
-    var val=ke.value;
-    val= val * 2.205;
+    var va=ke.value;
+    va= va * 2.205;
     var wee=document.getElementById('lbs');
-    wee.value=val;
+    wee.value=va;
 }
 var c=document.getElementById('weight');
 c.addEventListener('click',hc2);
+
+//Kilometer to Mile Conversion
+
+function hc3(){
+    var kaa=document.getElementById('km');
+    var valee=kaa.value;
+    valee= valee / 2.205;
+    var we=document.getElementById('m');
+    we.value=valee;
+}
+var c=document.getElementById('distance');
+c.addEventListener('click',hc3);
